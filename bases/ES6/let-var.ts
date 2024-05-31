41dffa33d6c91a7642d63598976fafc7230b0417
fersho101 <corral.fdo@gmail.com>
2024-05-31 08:58:20 -0700

    :construction:

---
+++ bases/ES6/let-var.ts
@@ -0,0 +1,17 @@
+;(() => {
+	console.log('let')
+
+	var a = 'Fernando'
+	console.log(a)
+
+	const NOMBRE = 'Corral'
+
+	const getName = (): void => {
+		console.log('Viejo getName')
+	}
+
+	// getName = () => {
+	// 	console.log('Nuevo var')
+	// }
+	// getName()
+})()
