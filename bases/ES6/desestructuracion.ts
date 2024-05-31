5967fe3f59f661e3e8ac9b295c78c2e8334b9efb
fersho101 <corral.fdo@gmail.com>
2024-05-31 09:31:08 -0700

    :construction: Practica desestructuracion

---
+++ bases/ES6/desestructuracion.ts
@@ -0,0 +1,42 @@
+;(() => {
+	type Avengers = {
+		nick: string
+		ironman: string
+		vison: string
+		activo: boolean
+		poder: number
+	}
+
+	const avengers: Avengers = {
+		nick: 'Samuel L. Jackson',
+		ironman: 'Robert Downy Jr',
+		vison: 'Paul Bettany',
+		activo: true,
+		poder: 1500.1201,
+	}
+
+	// const {poder, vison} = avengers
+
+	// console.log(poder, vison.toUpperCase())
+	// console.log(Number(poder.toFixed(2)))
+
+	const printAvengers = ({ ironman, vison, ...resto }: Avengers) => {
+		console.log(avengers.vison)
+		console.log(ironman)
+		console.log(vison)
+		console.log(resto)
+	}
+	// printAvengers(avengers)
+
+
+    const avengerArr: string[] = ['Capitan America', 'ironman', 'Hulk', 'Magneto']
+    // const ironman = avengerArr[1]
+    const [capi,ironman] = avengerArr
+    console.log({ironman, capi})
+
+
+
+
+
+
+})()
--- bases/main.js
+++ bases/main.js
@@ -1,4 +1,35 @@
 "use strict";
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+;
+(() => {
+    const avengers = {
+        nick: 'Samuel L. Jackson',
+        ironman: 'Robert Downy Jr',
+        vison: 'Paul Bettany',
+        activo: true,
+        poder: 1500.1201,
+    };
+    const printAvengers = (_a) => {
+        var { ironman, vison } = _a, resto = __rest(_a, ["ironman", "vison"]);
+        console.log(avengers.vison);
+        console.log(ironman);
+        console.log(vison);
+        console.log(resto);
+    };
+    const avengerArr = ['Capitan America', 'ironman', 'Hulk', 'Magneto'];
+    const [capi, ironman] = avengerArr;
+    console.log({ ironman, capi });
+})();
 ;
 (() => {
     console.log('let');
